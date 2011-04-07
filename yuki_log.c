2750609119cb8ddd60527a3a5d41a3faeaf8f9cf
jprosper <jprosper@163.com>
2011-04-07 11:38:14 +0800

    修改日志系统，添加多日志文件管理

--- yuki_log.c
+++ yuki_log.c
@@ -3,10 +3,12 @@
 #include <sys/types.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <stdarg.h>
 #include <errno.h>
+#include <pthread.h>
 
 #include "libconfig.h"
 #include "yuki.h"
@@ -16,10 +18,25 @@
 #define YLOG_CONFIG_PATH_MAX_LEVEL       YUKI_CONFIG_SECTION_YLOG "/max_level"
 #define YLOG_CONFIG_PATH_MAX_LINE_LENGTH YUKI_CONFIG_SECTION_YLOG "/max_line_length"
 
-static yint32_t g_ylog_max_level = YLOG_LEVEL_MAX;
-static ybool_t g_ylog_inited = yfalse;
-static FILE * g_ylog_file = NULL;
-static yint32_t g_ylog_max_log_line_length = YLOG_MAX_LINE_LENGTH;
+#define YUKI_CONFIG_SECTION_YSPECIAL     YUKI_CONFIG_SECTION_YLOG "/special"
+#define YLOG_CONFIG_LOG_SPECIAL_LEVEL    "level"
+#define YLOG_CONFIG_LOG_SPECIAL_FILE     "log_file"
+
+#define YLOG_MAX_PATH_LENGTH             2048
+
+static yint32_t     g_ylog_max_level = YLOG_LEVEL_MAX;
+static ybool_t      g_ylog_inited = yfalse;
+static FILE *       g_ylog_file = NULL;
+static yint32_t     g_ylog_max_log_line_length = YLOG_MAX_LINE_LENGTH;
+static char         g_ylog_real_file[YLOG_MAX_PATH_LENGTH];
+
+static char         g_ylog_dir[YLOG_MAX_PATH_LENGTH];
+// TODO: implement different log files for different level
+static FILE *       g_ylog_files[YLOG_LEVEL_MAX];
+static char         g_ylog_real_files[YLOG_LEVEL_MAX][YLOG_MAX_PATH_LENGTH];
+ysize_t             g_ylog_max_type;
+
+static pthread_key_t g_ylog_thread_key;
 
 static inline ybool_t ylog_inited()
 {
@@ -31,7 +48,6 @@ ybool_t _ylog_init(config_t * config)
     if (ylog_inited()) {
         return ytrue;
     }
-
     const char * log_dir;
     const char * log_file;
 
@@ -53,7 +69,7 @@ ybool_t _ylog_init(config_t * config)
             return yfalse;
         }
 
-        if (mkdir(log_dir, 0777)) {
+        if (mkdir(log_dir, 0751)) {
             YUKI_LOG_FATAL("cannot create dir '%s'. [errno: %d] [err: %m]", log_dir, errno);
             return yfalse;
         }
@@ -64,19 +80,23 @@ ybool_t _ylog_init(config_t * config)
         }
     }
 
-    ysize_t log_path_size = strlen(log_dir) + strlen(log_file) + 1;
-    char log_path[log_path_size];
-    snprintf(log_path, log_path_size, "%s%s", log_dir, log_file);
+    ysize_t ylog_path_size = strlen(log_dir) + strlen(log_file) + 1;
+    memset(g_ylog_real_file,0,YLOG_MAX_PATH_LENGTH);
+    if (ylog_path_size>YLOG_MAX_PATH_LENGTH) {
+        YUKI_LOG_FATAL("log dir '%s' is so long", log_dir);
+        return yfalse;
+    }
+    snprintf(g_ylog_real_file, ylog_path_size, "%s%s", log_dir, log_file);
 
-    g_ylog_file = fopen(log_path, "a");
-
+    memset(g_ylog_dir,0,YLOG_MAX_PATH_LENGTH);
+    memcpy(g_ylog_dir,log_dir,strlen(log_dir));
+    g_ylog_file = fopen(g_ylog_real_file, "a");
     if (!g_ylog_file) {
-        YUKI_LOG_FATAL("cannot open log path '%s' for write", log_path);
+        YUKI_LOG_FATAL("cannot open log path '%s' for write", g_ylog_real_file);
         return yfalse;
     }
 
     g_ylog_max_log_line_length = YLOG_MAX_LINE_LENGTH;
-    g_ylog_inited = ytrue;
 
     YUKI_LOG_DEBUG("%s is set to %s", YLOG_CONFIG_PATH_LOG_DIR, log_dir);
     YUKI_LOG_DEBUG("%s is set to %s", YLOG_CONFIG_PATH_LOG_FILE, log_file);
@@ -89,6 +109,55 @@ ybool_t _ylog_init(config_t * config)
         YUKI_LOG_DEBUG("%s is set to %d", YLOG_CONFIG_PATH_MAX_LEVEL, g_ylog_max_level);
     }
 
+    // read special settings for special levels
+    memset(g_ylog_files,0,sizeof(FILE *));
+    memset(g_ylog_real_files,0,YLOG_LEVEL_MAX*YLOG_MAX_PATH_LENGTH);
+    ysize_t i;
+    yint32_t index;
+    config_setting_t* ylog_setting=config_lookup(config, YUKI_CONFIG_SECTION_YSPECIAL);
+    if (ylog_setting) {
+        if (CONFIG_TYPE_LIST == config_setting_type(ylog_setting)) {
+            g_ylog_max_type=config_setting_length(ylog_setting);
+            for (i = 0; i < g_ylog_max_type; i++) {
+                config_setting_t* ylog_set=config_setting_get_elem(ylog_setting, i);
+                if (CONFIG_TRUE != config_setting_lookup_int(ylog_set, YLOG_CONFIG_LOG_SPECIAL_LEVEL, &index)) {
+                    YUKI_LOG_FATAL("cannot get '%s' in config file", YLOG_CONFIG_LOG_SPECIAL_LEVEL);
+                    return yfalse;
+                }
+                YUKI_LOG_DEBUG("get '%s' in config file '%d' ", YLOG_CONFIG_LOG_SPECIAL_LEVEL,index);
+                if (CONFIG_TRUE != config_setting_lookup_string(ylog_set, YLOG_CONFIG_LOG_SPECIAL_FILE, &log_file)) {
+                    YUKI_LOG_FATAL("cannot get '%s' in config file", YLOG_CONFIG_LOG_SPECIAL_FILE);
+                    return yfalse;
+                }
+                YUKI_LOG_DEBUG("get '%s' in config file '%s' ", YLOG_CONFIG_LOG_SPECIAL_FILE,log_file);
+                if (index<0 || index>YLOG_LEVEL_MAX) {
+                    YUKI_LOG_FATAL("get log level '%d' out of range", index);
+                    return yfalse;
+                }
+                ylog_path_size = strlen(log_dir) + strlen(log_file) + 1;
+                if (ylog_path_size>YLOG_MAX_PATH_LENGTH) {
+                    YUKI_LOG_FATAL("log dir '%s' is so long", log_dir);
+                    return yfalse;
+                }
+                snprintf(g_ylog_real_files[index], ylog_path_size, "%s%s", log_dir, log_file);
+                g_ylog_files[index] = fopen(g_ylog_real_files[index], "a");
+                if (!g_ylog_file) {
+                    YUKI_LOG_FATAL("cannot open log path '%s' for write", g_ylog_real_files[index]);
+                    return yfalse;
+                }
+            }
+        }
+    }
+
+    int error = pthread_key_create(&g_ylog_thread_key, NULL);
+
+    if (error) {
+        YUKI_LOG_FATAL("cannot create thread key for ylog. [err: %d]", error);
+        return yfalse;
+    }
+
+    g_ylog_inited = ytrue;
+
     return ytrue;
 }
 
@@ -99,23 +168,33 @@ void _ylog_clean_up()
 
 void _ylog_shutdown()
 {
-    ylog_flush();
+
+    ysize_t level;
+    for (level = 0; level < YLOG_LEVEL_MAX; level++) {
+        if (g_ylog_files[level] != NULL) {
+            ylog_flush(level);
+            fclose(g_ylog_files[level]);
+            g_ylog_files[level] = NULL;
+        }
+    }
 
     if (g_ylog_file) {
         fclose(g_ylog_file);
         g_ylog_file = NULL;
     }
 
+    int error = pthread_key_delete(g_ylog_thread_key);
+    if (error)
+    {
+        YUKI_LOG_FATAL("cannot clean g_ylog_thread_key. [err: %d]", error);
+    }
+
     g_ylog_inited = yfalse;
 }
 
-void _ylog_write(ylog_level_t level, const char * log_header, const char * pattern, ...)
+void _ylog_write(ylog_level_t level, yint32_t logid, const char * log_header, const char * pattern, ...)
 {
     FILE * log_file = stderr;
-
-    if (ylog_inited()) {
-        log_file = g_ylog_file;
-    }
 
     if (level <= g_ylog_max_level) {
         char buf[g_ylog_max_log_line_length + 2];
@@ -123,7 +202,7 @@ void _ylog_write(ylog_level_t level, const char * log_header, const char * patte
         ysize_t offset = 0;
         time_t t = time(NULL);
         offset += strftime(buf + offset, size - offset, "%Y-%m-%d %H:%M:%S ", localtime(&t));
-        offset += snprintf(buf + offset, size - offset, "%s ", log_header);
+        offset += snprintf(buf + offset, size - offset, "%s [logid:%d] ", log_header,logid);
 
         va_list args;
         va_start(args, pattern);
@@ -132,13 +211,99 @@ void _ylog_write(ylog_level_t level, const char * log_header, const char * patte
 
         buf[offset++] = '\n';
         buf[offset++] = '\0';
+
+        if (ylog_inited()) {
+            log_file = g_ylog_file;
+            if (g_ylog_files[level] != NULL) {
+                log_file = g_ylog_files[level];
+            }
+        }
+
         fputs(buf, log_file);
     }
 }
 
-void ylog_flush()
+void ylog_flush(ylog_level_t level)
 {
     if (ylog_inited()) {
-        fflush(g_ylog_file);
+        if (g_ylog_files[level] != NULL) {
+            fflush(g_ylog_files[level]);
+        }else{
+            fflush(g_ylog_file);
+        }
     }
 }
+
+void ylog_rotate()
+{
+    YUKI_LOG_DEBUG("ylog_rotate");
+    if (ylog_inited()) {
+        struct stat stat_buf;
+        if (stat(g_ylog_dir, &stat_buf)) {
+            if (ENOENT != errno) {
+                YUKI_LOG_FATAL("cannot lstat dir '%s'. [errno: %d] [err: %m]", g_ylog_dir, errno);
+                return;
+            }
+
+            if (mkdir(g_ylog_dir, 0751)) {
+                YUKI_LOG_FATAL("cannot create dir '%s'. [errno: %d] [err: %m]", g_ylog_dir, errno);
+                return;
+            }
+        } else {
+            if (!S_ISDIR(stat_buf.st_mode)) {
+                YUKI_LOG_FATAL("log dir '%s' exists but it's not a dir", g_ylog_dir);
+                return;
+            }
+        }
+        FILE * log_file=NULL;
+        FILE * old_file=NULL;
+
+        if (g_ylog_file) {
+            log_file = fopen(g_ylog_real_file, "a");
+            if (!log_file) {
+                YUKI_LOG_FATAL("cannot rotate log file. [path: %s]", g_ylog_real_file);
+                return;
+            }
+            YUKI_LOG_DEBUG("log_file=%d g_ylog_path=%s g_ylog_file=%d",log_file,g_ylog_real_file,g_ylog_file);
+            old_file = g_ylog_file;
+            g_ylog_file = log_file;
+            fclose(old_file);
+        }
+        ysize_t level;
+
+        for (level = 0; level < YLOG_LEVEL_MAX; level++) {
+            if (g_ylog_files[level] != NULL) {
+                log_file = fopen(g_ylog_real_files[level], "a");
+                YUKI_LOG_DEBUG("level=%d log_file=%d g_ylog_path=%s g_ylog_file=%d",level,log_file,g_ylog_real_files[level],g_ylog_files[level]);
+                if (!log_file) {
+                    YUKI_LOG_FATAL("cannot rotate log file. [path: %s]", g_ylog_real_files[level]);
+                    return;
+                }
+                old_file = g_ylog_files[level];
+                g_ylog_files[level] = log_file;
+                fclose(old_file);
+            }
+        }
+    }
+}
+
+
+yint32_t ylog_get_pthread_key()
+{
+    intptr_t value = (intptr_t)pthread_getspecific(g_ylog_thread_key);
+    if (value == 0)
+    {
+        ylog_set_pthread_key();
+        value = (intptr_t)pthread_getspecific(g_ylog_thread_key);
+    }
+    return value;
+}
+
+yint32_t ylog_set_pthread_key()
+{
+    intptr_t value=(intptr_t)rand();
+    pthread_setspecific(g_ylog_thread_key,(void*)value);
+    return value;
+}
+
+
--- yuki_log.h
+++ yuki_log.h
@@ -8,12 +8,13 @@ extern "C" {
 #define _YLOG_FORMAT(prefix, file, line) _YLOG_FORMAT_REAL(prefix, file, line)
 #define _YLOG_FORMAT_REAL(prefix, file, line) "["prefix"] ["file":"#line"]"
 
-#define YUKI_LOG_CRITICAL(...) _ylog_write(YLOG_LEVEL_CRITICAL, _YLOG_FORMAT("CRITICAL", __FILE__, __LINE__), __VA_ARGS__)
-#define YUKI_LOG_FATAL(...)    _ylog_write(YLOG_LEVEL_FATAL,    _YLOG_FORMAT("FATAL", __FILE__, __LINE__), __VA_ARGS__)
-#define YUKI_LOG_WARNING(...)  _ylog_write(YLOG_LEVEL_WARNING,  _YLOG_FORMAT("WARNING", __FILE__, __LINE__), __VA_ARGS__)
-#define YUKI_LOG_NOTICE(...)   _ylog_write(YLOG_LEVEL_NOTICE    _YLOG_FORMAT("NOTICE", __FILE__, __LINE__), __VA_ARGS__)
-#define YUKI_LOG_TRACE(...)    _ylog_write(YLOG_LEVEL_TRACE,    _YLOG_FORMAT("TRACE", __FILE__, __LINE__), __VA_ARGS__)
-#define YUKI_LOG_DEBUG(...)    _ylog_write(YLOG_LEVEL_DEBUG,    _YLOG_FORMAT("DEBUG", __FILE__, __LINE__), __VA_ARGS__)
+#define YUKI_LOG_CRITICAL(...) _ylog_write(YLOG_LEVEL_CRITICAL, ylog_get_pthread_key(), _YLOG_FORMAT("CRITICAL", __FILE__, __LINE__), __VA_ARGS__)
+#define YUKI_LOG_FATAL(...)    _ylog_write(YLOG_LEVEL_FATAL,    ylog_get_pthread_key(), _YLOG_FORMAT("FATAL", __FILE__, __LINE__), __VA_ARGS__)
+#define YUKI_LOG_WARNING(...)  _ylog_write(YLOG_LEVEL_WARNING,  ylog_get_pthread_key(), _YLOG_FORMAT("WARNING", __FILE__, __LINE__), __VA_ARGS__)
+#define YUKI_LOG_NOTICE(...)   _ylog_write(YLOG_LEVEL_NOTICE,   ylog_get_pthread_key(), _YLOG_FORMAT("NOTICE", __FILE__, __LINE__), __VA_ARGS__)
+#define YUKI_LOG_TRACE(...)    _ylog_write(YLOG_LEVEL_TRACE,    ylog_get_pthread_key(), _YLOG_FORMAT("TRACE", __FILE__, __LINE__), __VA_ARGS__)
+#define YUKI_LOG_DEBUG(...)    _ylog_write(YLOG_LEVEL_DEBUG,    ylog_get_pthread_key(), _YLOG_FORMAT("DEBUG", __FILE__, __LINE__), __VA_ARGS__)
+
 
 #define YLOG_MAX_LINE_LENGTH 1024
 
@@ -27,8 +28,11 @@ typedef enum _ylog_level_t {
     YLOG_LEVEL_MAX,
 } ylog_level_t;
 
-void _ylog_write(ylog_level_t level, const char * log_header, const char * pattern, ...);
-void ylog_flush();
+void _ylog_write(ylog_level_t level, yint32_t logid, const char * log_header, const char * pattern, ...);
+void ylog_flush(ylog_level_t level);
+void ylog_rotate();
+yint32_t ylog_get_pthread_key();
+yint32_t ylog_set_pthread_key();
 
 #ifdef __cplusplus
 }
